5e7516ca312c5f680b1bb922e6c6705abbdaf5cd
Ilya udalov <ilya_udalov_04@mail.ru>
2023-10-16 22:45:37 +0300

    update lab 3 first part task

---
+++ src/main/java/org/estoutic/solution_three/SolutionThree.java
@@ -0,0 +1,83 @@
+package org.estoutic.solution_three;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class SolutionThree {
+    public static void main(String[] args) {
+
+//        randomGenerate();
+       checkUpRow();
+    }
+
+    public static void randomGenerate(){
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.print("Enter arrays size: ");
+        int arrayLength = scanner.nextInt();
+
+        double[] firstRandomArray = new double[arrayLength];
+        double[] secondRandomArray = new double[arrayLength];
+
+
+        for (int i = 0; i < arrayLength; i++) {
+            firstRandomArray[i] = Math.random();
+        }
+
+        for (int i = 0; i < arrayLength; i++) {
+            secondRandomArray[i] = rand.nextDouble();
+        }
+
+        System.out.println("\nFirst not sorted Array: ");
+        printArray(firstRandomArray);
+
+        System.out.println("\nSecond not sorted Array: ");
+        printArray(secondRandomArray);
+
+        Arrays.sort(firstRandomArray);
+        Arrays.sort(secondRandomArray);
+
+        System.out.println("\nFirst sorted Array: ");
+        printArray(firstRandomArray);
+
+        System.out.println("\nSecond sorted Array: ");
+        printArray(secondRandomArray);
+    }
+
+    public static void printArray(double[] arr) {
+        for (double element : arr) {
+            System.out.println(element);
+        }
+    }
+
+    public static void checkUpRow(){
+        int arrayLength = 4;
+        int[] randomArray = new int[arrayLength];
+        Random rand = new Random();
+
+        for (int i = 0; i < arrayLength; i++) {
+            randomArray[i] = rand.nextInt(90) + 10;
+        }
+
+        System.out.print("Array: \n");
+        for (int i = 0; i < arrayLength; i++) {
+            System.out.print(randomArray[i] + " ");
+        }
+
+        boolean isIncreasing = true;
+        for (int i = 1; i < arrayLength; i++) {
+            if (randomArray[i] <= randomArray[i - 1]) {
+                isIncreasing = false;
+                break;
+            }
+        }
+
+        if (isIncreasing) {
+            System.out.println("\n True ");
+        } else {
+            System.out.println("\n False ");
+        }
+    }
+}
